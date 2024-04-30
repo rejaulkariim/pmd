a4584d7dc69437054948907dfcb5f4beb4a19f46
karimscoding <rejaulkarim66666@gmail.com>
2024-04-30 21:41:41 +0800

    ci: enable tailwind merge utils and a wrapper component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import MaxWidthWrapper from "@/components/shared/MaxWidthWrapper";
 import { Button } from "antd";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Button type="primary">Button</Button>
+      <MaxWidthWrapper>
+        <h1>Hello</h1>
+        <Button type="primary">Button</Button>
+      </MaxWidthWrapper>
     </div>
   );
 }
---
+++ src/components/shared/MaxWidthWrapper.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn(`max-w-screen-xl mx-auto px-10 md:px-20`, className)}>
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
