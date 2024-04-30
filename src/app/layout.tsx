3508fc93353eb5481ab1546a3094479ff464763c
karimscoding <rejaulkarim66666@gmail.com>
2024-04-30 21:31:54 +0800

    ci: enable antd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -15,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "antd";
+
 export default function Home() {
   return (
     <div>
       <h1>Hello</h1>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
